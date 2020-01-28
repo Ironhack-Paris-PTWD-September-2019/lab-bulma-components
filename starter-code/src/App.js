2eeac9e4b00a5788c9fd0c785b2c6e55cc333bb1
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2020-01-28 20:59:11 +0100

    Iteration 1

---
+++ starter-code/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
