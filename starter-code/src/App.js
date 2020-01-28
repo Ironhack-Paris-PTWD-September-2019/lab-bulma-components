1d93db16b495b2415c61161fd00902a8714bdc16
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2020-01-28 21:07:12 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar.js";
+import FormField from "./FormField.js";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return <Navbar />;
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="form">
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField
+            label="Email"
+            type="email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
