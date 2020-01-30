e5c4d75d5baa95f2568a6624100874e39f0c392e
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-01-30 19:35:46 +0100

    up to iteration 4

---
+++ starter-code/src/CoolButton.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class CoolButton extends Component {
+
+    state = {
+        isActive: 'is-active',
+        isBlack: 'is-black',
+        isCentered: 'is-centered',
+        isDanger: 'is-danger',
+        isDark: 'is-dark',
+        isFocused: 'is-focused',
+        isGrouped: 'is-grouped',
+        isHovered: 'is-hovered',
+        isInfo: 'is-info',
+        isInverted: 'is-inverted',
+        isLarge: 'is-large',
+        isLight: 'is-light',
+        isLink: 'is-link',
+        isLoading: 'is-loading',
+        isMedium: 'is-medium',
+        isOutlined: 'is-outlined',
+        isPrimary: 'is-primary',
+        isRight: 'is-right',
+        isRounded: 'is-rounded',
+        isSelected: 'is-selected',
+        isSmall: 'is-small',
+        isStatic: 'is-static',
+        isSuccess: 'is-success',
+        isText: 'is-text',
+        isWarning: 'is-warning',
+        isWhite: 'is-white',
+        }
+
+
+    render() {
+        return (
+            
+
+            <div class="buttons">
+                <button class="button is-rounded my-class is-danger is-small">Sign in</button>
+
+                <button class="button is-small is-success">Sign up</button>
+
+            </div>
+    
+
+        )
+    }
+}
+
+
+
+
+export default CoolButton
